5cab8d90448bc551a88486ec206678f7ecee6a69
ViyeiraAlmaz <156874246+ViyeiraAlmaz@users.noreply.github.com>
2024-02-07 03:00:45 +0600

    Update functions3.py

--- lab3/functions3.py
+++ lab3/functions3.py
@@ -11,20 +11,68 @@ class Stringj:
 JJ = Stringj()
 JJ.getString()
 JJ.printString()
-#6 ex
-def is_prime(n):
-    if n <= 1:
-        return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
+#2 ex
+class Shape:
+    def init(self):
+        pass
+    def area(self):
+        print("area of the shape:", 0)
+class Square(Shape):
+    def init(self, length):
+        super().init()
+        self.length = length
+    def area(self):
+        print("area of the square:", self.length ** 2)
+shape = Shape()
+shape.area()
+square = Square(int(input()))
+square.area() 
 
-numbers = list(map(int, input().split()))
+#3 ex
 
-prime_numbers = list(filter(lambda x: is_prime(x), numbers))
+class Shape:
+    def init(self):
+        pass
+    def area(self):
+        print("Area of the shape:", 0)
+class Rectangle(Shape):
+    def init(self, length, width):
+        super().init()
+        self.length = length
+        self.width = width
+    
+    def area(self):
+        print("area of the rectangle:", self.length * self.width)
+length=float(input())
+width=float(input())
+rectangle = Rectangle(length, width)
+rectangle.area()
 
-print(prime_numbers)
+#4 ex
+
+import math
+class Point:
+    def init(self, x, y):
+        self.x = x
+        self.y = y
+    def show(self):
+        print(f"Coordinates of the point: ({self.x}, {self.y})")
+    def move(self, new_x, new_y):
+        self.x = new_x
+        self.y = new_y
+    def dist(self, other_point):
+        distance = math.sqrt((self.x - other_point.x)**2 + (self.y - other_point.y)**2)
+        return distance
+x1=int(input())
+y1=int(input())
+point1=Point(x1,y1)
+x2=int(input())
+y2=int(input())
+point2 = Point(x2,y2)
+point1.show()
+point2.show()
+distance = point1.dist(point2)
+print("Distance between point1 and point2:", distance)
 #5 ex
 class Account:
     def __init__(s, owner, balance=0):
