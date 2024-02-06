4c871789043abe29b1a841621ece9ce7179aa5ac
ViyeiraAlmaz <156874246+ViyeiraAlmaz@users.noreply.github.com>
2024-02-07 01:39:18 +0600

    Add files via upload

---
+++ lab3/functions1.py
@@ -0,0 +1,22 @@
+#ex 1
+def ounces(x):
+    print( x * 28.3495231)
+
+
+a = float(input())
+ounces(a)
+
+#ex 2
+def Celcium(x):
+    c = (5 / 9) * (x - 32)
+    print(c)
+
+x = int(input())
+Celcium(x)
+
+#ex3
+def solve(numheads, numlegs):
+    
+    
+numheads = int(input())
+numlegs = int(input())
