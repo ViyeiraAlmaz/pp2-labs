a0bd794bb12fe672c5b8f1482739347e352b1708
ViyeiraAlmaz <156874246+ViyeiraAlmaz@users.noreply.github.com>
2024-03-08 23:56:39 +0600

    Update builtin.py

--- lab_6/builtin.py
+++ lab_6/builtin.py
@@ -1,16 +1,41 @@
-from functools import reduce
+#ex1
+import math, time
+def llist(list1):
+    return math.prod(list1)
+print(llist([1,2,3,4,5]))
+print()
 
-def multiply_list(numbers):
-    if not numbers:
-        return None  # Return None if the list is empty
-    else:
-        return reduce(lambda x, y: x * y, numbers)
+#ex2
+def strins(strs):
+    upperc=sum(1 for ch in strs if ch.isupper())
+    lowerc=sum(1 for ch in strs if ch.islower())
+    return upperc,lowerc
+strs="This is NOT AvAible"
+upperc,lowerc=strins(strs)
+print("This is UPPERCASES:>",upperc)
+print("This is lowercases:>",lowerc)
+print()
 
-# Example list of numbers
-numbers = [1, 2, 3, 4, 5]
+#ex3
+def stringgs(strs):
+    return "True" if strs==strs[::-1] else "False"
+print(stringgs("abba"))
+print(stringgs("abbd"))
+print()
 
-result = multiply_list(numbers)
-if result is not None:
-    print("Result of multiplying all numbers in the list:", result)
-else:
-    print("The list is empty.")
+#ex4
+def root_after_ms(num, ms):
+    time.sleep(ms / 1000)
+    return math.sqrt(num)
+num = 25100
+ms = 2123
+print(f"Square root of {num} after {ms} miliseconds is {root_after_ms(num, ms)}")
+print()
+
+#ex5
+def thisistuple(tupl):
+    x=all(tupl)
+    return x
+print(thisistuple((1,2,3,4)))
+print(thisistuple((False,True)))
+print(thisistuple(("thisisstring",1234)))
